a35d764f4d166e6c0c38e1b882669fd94b5a578c
wangzunshi <2232117623@qq.com>
2026-02-09 17:20:39 +0800

    【Cursor重构】main.py文件结构和可读性优化

--- main.py
+++ main.py
@@ -9,69 +9,102 @@ from testModules.pyInstallAPK import install_latest_apk
 from testModules.pySaveLog import save_log
 from Scripts.loadJson import load_conf
 
-choice = ''
-
 
 def get_adb_path():
     current_path = os.path.dirname(os.path.abspath(sys.argv[0]))
-    adb_path = os.path.join(current_path, 'adb', 'adb.exe')
+    adb_path = os.path.join(current_path, "adb", "adb.exe")
     return adb_path
+
+
+def build_project_maps(config):
+    """
+    根据配置构建项目相关的映射：
+    - project_names: 项目名列表
+    - package_name:  项目 -> 包名
+    - package_type:  项目 -> apk 类型
+    - package_path:  项目 -> apk 所在目录
+    """
+    projects = config.get("project", {})
+    if not isinstance(projects, dict):
+        raise ValueError("`project` 字段在 config.json 中必须是一个字典")
+
+    project_names = list(projects.keys())
+    package_name = {k: v["packagename"] for k, v in projects.items()}
+    package_type = {k: v["apk_type"] for k, v in projects.items()}
+    package_path = {k: v["pkg_folder"] for k, v in projects.items()}
+
+    return project_names, package_name, package_type, package_path
+
+
+def handle_user_choice(selection, config, package_type, package_path, adb_path):
+    """
+    根据用户在交互界面中的选择执行对应逻辑。
+    """
+    choice = selection["choice"]
+
+    if choice == "InstallLatest":
+        install_latest_apk(
+            adb_path=adb_path,
+            _apk_type=package_type[selection["project"]],
+            project_name=selection["project"],
+            device=selection["devices"],
+            pkg_folder=package_path,
+        )
+    elif choice in ("AllInfo", "BattleInfo"):
+        printLog(
+            adb_path=adb_path,
+            device=selection["devices"],
+            pkg_name=selection["packagename"],
+            fileter=selection["filter"],
+            keyword=selection["keyword"],
+        )
+    elif choice == "SaveLog":
+        current_time = datetime.datetime.now().strftime("%m%d-%H%M")
+        current_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+        file_name = f"{selection['devices']}_{current_time}.log"
+        file_path = os.path.join(current_dir, file_name)
+
+        save_log(
+            adb_path=adb_path,
+            device=selection["devices"],
+            filtered_word=config["filtered_words"][0],
+            filename=file_name,
+            timeout=10,
+        )
+        # saveLog(adb_path=adb_path, device=selection["devices"], filtered_word=config["filtered_words"][0],
+        #         filename=file_path, pkg_name=selection["packagename"])
+
+    return choice
 
 
 def main():
     # 加载配置并加工成函数需要的字典
     config = load_conf()
-    proj_list = config["project"].keys()
 
-    pkg_name, pkg_type, pkg_path = [], [], []
+    project_names, package_name, package_type, package_path = build_project_maps(config)
 
-    if isinstance(config["project"], dict):
-        for key, value in config["project"].items():
-            pkg_name.append(value["packagename"])
-            pkg_type.append(value["apk_type"])
-            pkg_path.append(value["pkg_folder"])
+    # 创建 CMD 窗口并提供设置的选项
+    selection = create_frame(
+        packagename=package_name,
+        keyword=config["keyword"],
+        project=project_names,
+        filtered_words=config["filtered_words"],
+    )
 
-    package_name = {k: v for k, v in zip(proj_list, pkg_name)}
-    package_type = {k: v for k, v in zip(proj_list, pkg_type)}
-    package_path = {k: v for k, v in zip(proj_list, pkg_path)}
+    adb_path = get_adb_path()
 
-    # 创建CMD窗口并提供设置的选项
-    res = create_frame(packagename=package_name, keyword=config["keyword"], project=proj_list,
-                       filtered_words=config["filtered_words"])
-
-    # print(res)
-
-    global choice
-    choice = res['choice']
-
-    _adb_path = get_adb_path()
-
-    # 根据选择装包或者拉log决定逻辑
-    if res['choice'] == 'InstallLatest':
-        install_latest_apk(adb_path=_adb_path, _apk_type=package_type[res['project']], project_name=res['project'],
-                           device=res['devices'], pkg_folder=package_path)
-    elif res['choice'] == 'AllInfo' or res['choice'] == 'BattleInfo':
-        printLog(adb_path=_adb_path, device=res['devices'], pkg_name=res['packagename'], fileter=res['filter'],
-                 keyword=res['keyword'])
-    elif res['choice'] == 'SaveLog':
-        current_time = datetime.datetime.now().strftime("%m%d-%H%M")
-        current_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
-        file_name = str(res["devices"]) + f"_{current_time}.log"
-        file_path = os.path.join(current_dir,file_name)
-        save_log(adb_path=_adb_path, device=res["devices"], filtered_word=config["filtered_words"][0],
-                 filename=str(res["devices"]) + f"_{current_time}.log", timeout=10)
-        # saveLog(adb_path=_adb_path, device=res["devices"], filtered_word=config["filtered_words"][0],
-        # filename=file_path, pkg_name=res['packagename'])
+    # 根据选择装包或者拉 log 决定逻辑
+    return handle_user_choice(selection, config, package_type, package_path, adb_path)
 
 
 if __name__ == '__main__':
     try:
-        main()
+        choice = main()
         count_down(seconds=10)
     except KeyboardInterrupt as e:
         print(e)
     finally:
-        if choice == 'InstallLatest' or "SaveLog":
+        if choice in ('InstallLatest', 'SaveLog'):
             count_down(seconds=10)
         else:
             count_down(minutes=1)
